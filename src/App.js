ed8839b8932271b222838a625f8e6a9f9315ae80
Needforock2 <needforock@gmail.com>
2023-04-06 21:57:24 -0400

    navbar styles

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
+      <header className="App-header col-12 justify-content-start">
+        <div className='col-6'>
+          <h1>Cachupines</h1>
+        </div>
+        <div className='col-6 d-flex flex-row justify-content-end align-items-center'>          
+          <button className='btn btn-light'>Ropa</button>
+          <button className='btn btn-light'>Accesorios</button>
+          <button className='btn btn-light'>Menaje</button>
+          <button className='btn btn-light'> <i className="bi bi-cart"></i> Carrito</button>
+        </div>  
+        
       </header>
+      
+
     </div>
   );
 }
